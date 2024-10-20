a3c29f2aa06c51a7f7efc04c9380451e71c6fab8
Gregory Valdez <valdezga@uci.edu>
2024-10-19 21:11:39 -0700

    10/19: Login now checks customer table to validate login, also created a test user user/password test@uci.edu/123456

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -1,44 +1,75 @@
 import com.google.gson.JsonObject;
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
 public class LoginServlet extends HttpServlet {
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
+    private static final long serialVersionUID = 1L;
+    private DataSource dataSource;
+
+    public void init(ServletConfig config ) {
+        try{
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedbexample");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-
-        /* This example only allows username/password to be test/test
-        /  in the real project, you should talk to the database to verify username/password
-        */
         JsonObject responseJsonObject = new JsonObject();
-        if (email.equals("test@test.com") && password.equals("123456")) {
-            // Login success:
 
-            // set this user into the session
+        if (email.equals("test@uci.edu") && password.equals("123456")) {
+            //test login successfull
             request.getSession().setAttribute("user", new User(email));
-
             responseJsonObject.addProperty("status", "success");
-            responseJsonObject.addProperty("message", "success");
-
+            responseJsonObject.addProperty("message", "Success");
+            System.out.println("Test user login success: " + email);
         } else {
-            // Login fail
-            responseJsonObject.addProperty("status", "fail");
-            // Log to localhost log
-            request.getServletContext().log("Login failed");
-            // sample error messages. in practice, it is not a good idea to tell user which one is incorrect/not exist.
-            if (!email.equals("test@test.com")) {
-                responseJsonObject.addProperty("message", "email " + email + " doesn't exist");
-            } else {
-                responseJsonObject.addProperty("message", "incorrect password");
+            request.getServletContext().log("Test user login fail: " + email);
+            try (Connection con = dataSource.getConnection()) {
+                String query = "SELECT * FROM customers WHERE email = ? AND password = ?";
+                PreparedStatement ps = con.prepareStatement(query);
+                ps.setString(1, email);
+                ps.setString(2, password);
+
+                ResultSet rs = ps.executeQuery();
+
+                if (rs.next()) {
+                    request.getSession().setAttribute("user", new User(email));
+                    responseJsonObject.addProperty("status", "success");
+                    responseJsonObject.addProperty("message", "success");
+                } else {
+                    // Login fail
+                    responseJsonObject.addProperty("status", "fail");
+                    responseJsonObject.addProperty("message", "Invalid email or password");
+                    System.out.println("Login Failed: " + email);
+                }
+            } catch (Exception e) {
+                responseJsonObject.addProperty("status", "error");
+                responseJsonObject.addProperty("message", "Internal Server Error");
+                System.out.println("Internal Server Error");
+                //System.out.println(e.printStackTrace().toString());
             }
         }
+
         response.getWriter().write(responseJsonObject.toString());
     }
 }
